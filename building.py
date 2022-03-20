d469b396b52baed29ebac6da7ece2c47f999d29b
Metodi Angelov <101995681+Ubersicka@users.noreply.github.com>
2022-03-20 23:40:49 +0200

    Create building.py

---
+++ building.py
@@ -0,0 +1,14 @@
+floors = int(input())
+rooms = int(input())
+
+for f in range(floors, 0, -1):
+    for r in range(rooms):
+
+        if f == floors:
+            print(f'L{f}{r}', end=" ")
+        elif f % 2 == 0:
+            print(f'O{f}{r}', end=" ")
+        elif f % 2 != 0:
+            print(f'A{f}{r}', end=" ")
+
+    print()       #Сваля курсора на следващия ред, преди новия етаж.
